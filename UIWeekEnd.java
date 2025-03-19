3436b81ae59f10cec14f39e3f428ce7cccd2c4b0
MohamedMIQYASS <mohamed.miqyass1@etu.univ-orleans.fr>
2025-03-19 10:15:34 +0100

    regroupement

--- UIWeekEnd.java
+++ UIWeekEnd.java
@@ -2,6 +2,102 @@ package TP4;
 import java.util.List; 
 import java.util.ArrayList;
 
+class Personne{
+    private String nom;
+    private String prenom;
+    public Personne( String n, String p) {this.nom = n; this.prenom = p;}
+    public String toString() {return this.prenom + " " + this.nom + " ";  }
+    public String getNom() {return this.nom;}
+    public String getPrenom() {return this.prenom;}
+}
+
+class Depense{
+    private Personne pers;
+    private double montant; 
+    private String produit;
+    public Depense(Personne p, double mont, String prod ){
+	this.pers = p; this.montant = mont; this.produit = prod;
+    }
+    public Personne getPersonne() {return this.pers;}
+    public double getMontant() {return this.montant;}
+    public String getProduit() {return this.produit;}
+
+	public String toString() {
+		String res = new String();
+		res += "{" + this.getPersonne();
+		res += " : " + this.getProduit();
+		res +=  " , " + this.getMontant() + "euros";
+		return res;
+ 	}
+}
+
+
+class WeekEnd{
+    private List<Personne> listeAmis;
+    private List<Depense> listeDepenses;
+
+    public WeekEnd(){
+		this.listeAmis = new ArrayList<>();
+		this.listeDepenses = new ArrayList<>();
+    }
+	
+    public List<Personne> getAmis() {
+		return this.listeAmis;
+    }
+
+    public List<Depense> getDepenses() {
+		return this.listeDepenses;
+    }
+    
+    public void ajouterPersonne(Personne p){this.listeAmis.add(p);}
+    
+	public void ajouterDepense(Depense d){this.listeDepenses.add(d); }
+    
+	// totalDepensesPersonne prend en entrée une personne et renvoie la somme des depenses de cette personne.
+    public double totalDepensesPersonne(Personne p){
+		double s = 0.0;
+		for(Depense d : this.listeDepenses) {
+			if((p.getNom()).equals(d.getPersonne().getNom()))
+			s += d.getMontant();
+		}
+		return s;
+    }
+
+    // totalDepenses renvoie la somme de toutes les dépenses. 
+    public double totalDepenses(){
+		double s = 0.0; 
+		for(Depense d : this.listeDepenses) 
+			s += d.getMontant();
+		return s;
+    }
+
+    // depensesMoyenne renvoie la moyenne des dépenses pour une personne 
+    public double depensesMoyenne(){
+		double s = totalDepenses(); 
+		int nb = this.listeAmis.size(); 
+		if(nb != 0) 
+			return s/nb;
+		else 
+			return s; 
+    }
+
+    //depenseProduit prend en entrée un produit, et renvoie la somme des dépenses pour ce produit. (par exemple du pain peut avoir été acheté plusieurs fois..)
+    public double depenseProduit(String p){
+		double s = 0.0; 
+		for(Depense d : this.listeDepenses) 
+			if (p.equals(d.getProduit()))
+			s+= d.getMontant();
+		return s;
+    } 
+
+    // avoirPersonne prend en entrée une personne et renvoie l'avoir de cette personne pour le week end.
+    public double avoirPersonne(Personne p){
+		double m = depensesMoyenne(); 
+		double avoir = m - totalDepensesPersonne(p); 
+		return avoir; 
+    }
+}
+
 
 public class UIWeekEnd {
 
@@ -58,11 +154,11 @@ public class UIWeekEnd {
         boolean commande_faite = false;
         while(!commande_faite) {
             System.out.println("Que voulez vous faire?");
-            System.out.println("P : Afficher la liste des personnes");
+            System.out.println("P : afficher les personnes du week-end");
             System.out.println("Q: quitter");
-            System.out.println("D: Afficher la liste des dépenses");
-            System.out.println("T: Afficher le total des dépenses");
-            System.out.println("M: Afficher la moyenne des dépenses");
+            System.out.println("D: afficher les dépenses du week-end");
+            System.out.println("T: afficher le total des dépenses du week-end");
+            System.out.println("M: afficher la dépense moyenne par personne pour le week-end.");
             String commande_brute = System.console().readLine();
             String commande = commande_brute.strip().toLowerCase();
             if(commande.equals("q")) {
@@ -78,25 +174,30 @@ public class UIWeekEnd {
                 System.out.println("");
             } 
             else if (commande.equals("d")){
-                List<Personne> personnes = this.we.getAmis();
+                List<Depense> depense = this.we.getDepenses();
                 System.out.println("les dépenses sont :");
-                for ( Personne p : personnes){
-                    System.out.println("\t" + d );
+                for ( Depense d : depense){
+                    System.out.println("\t" +d );
                 }
                 System.out.println("");
             }
-           else if (commande.equals("t")){
-            List<Personne> personnes = this.we.getAmis();
-            System.out.println("Total des dépenses :");
-            for ( Personne p : personnes){
-                System.out.println("\t" +t );
+            else if (commande.equals("t")){
+                double totalDepenses = this.we.totalDepenses();
+                System.out.println("Total des dépenses :");
+                for ( Depense p : depense){
+                    System.out.println("\t" +t );
+                }
+                System.out.println("");
             }
-            System.out.println("");
-
-        }
-
+            else if (commande.equals("m")){
+                double depensesMoyenne = this.we.depensesMoyenne();
+                System.out.println("La moyenne des dépenses :");
+                for ( Depense d : depense){
+                    System.out.println("\t" +m );
+                }
+                System.out.println("");
+            }
     }
-
 }
         /// Affiche un message de bienvenue
         public void bienvenue() {
